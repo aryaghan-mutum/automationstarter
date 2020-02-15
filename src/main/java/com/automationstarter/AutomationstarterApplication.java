2054bd5578faca5b5cde4f1bb5e1ec011abeed27
Anurag Muthyam <amuthyam@rccl.com>
2020-02-15 12:09:48 -0500

    first commit

---
+++ src/main/java/com/automationstarter/AutomationstarterApplication.java
@@ -0,0 +1,13 @@
+package com.automationstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationstarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automationstarter/AutomationstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automationstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
